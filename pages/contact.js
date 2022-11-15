2d5346b02fa82b37f8c5514319a54493c7bfe1c4
Mario Gilezan <mariogilezan10@gmail.com>
2022-11-15 21:38:45 +0100

    add styles on main page

---
+++ pages/contact.js
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+
+export default function Contact() {
+  return (
+    <div>
+      <div>
+        <nav>
+          <div>
+            <h1>
+              <Link href='/'>Food Ninja</Link>
+            </h1>
+          </div>
+          <ul>
+            <li>
+              <Link href='/'>
+                <span>Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link href='/about'>
+                <span>About</span>
+              </Link>
+            </li>
+            <li>
+              <Link href='/contact'>
+                <span>Contact</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>{' '}
+      {/* end nav */}
+      <main className='mt-20'>
+        <h1 className='text-4xl text-green-500'>Contact</h1>
+      </main>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,18 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        },
+      },
+      fontFamily: {
+        body: ['Nunito'],
+      },
+    },
   },
   plugins: [],
 };
