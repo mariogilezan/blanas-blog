288033c852bd3c5c62f99774655815883ae610ae
Mario Gilezan <mariogilezan10@gmail.com>
2022-11-30 21:03:11 +0100

    add empty about and contact pages

--- pages/about.js
+++ pages/about.js
@@ -1,38 +1,9 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 
 export default function About() {
   return (
     <div>
-      <div>
-        <nav>
-          <div>
-            <h1>
-              <Link href='/'>Food Ninja</Link>
-            </h1>
-          </div>
-          <ul>
-            <li>
-              <Link href='/'>
-                <span>Home</span>
-              </Link>
-            </li>
-            <li>
-              <Link href='/about'>
-                <span>About</span>
-              </Link>
-            </li>
-            <li>
-              <Link href='/contact'>
-                <span>Contact</span>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>{' '}
-      {/* end nav */}
-      <main className='mt-20'>
-        <h1 className='text-4xl text-red-500'>About</h1>
-      </main>
+      <h1>About</h1>
     </div>
   );
 }
