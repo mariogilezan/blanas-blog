cd23f649406291994f9c0122ee1a7ead3c0a7a47
Mario Gilezan <mariogilezan10@gmail.com>
2022-11-30 20:56:46 +0100

    add custom values in tailwind and next config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        primary: '#FF6363',
-        secondary: {
-          100: '#E2E2D5',
-          200: '#888883',
-        },
+        gray: colors.neutral,
       },
       fontFamily: {
-        body: ['Nunito'],
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        stock: [defaultTheme.fontFamily.sans],
+      },
+      aspectRatio: {
+        '4/3': '4 / 3',
+        '3/2': '3 / 2',
+        '2/3': '2 / 3',
+        '9/16': '9 / 16',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+  ],
 };
