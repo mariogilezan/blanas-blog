ff005ae166df09148596713bf7ab5314a54c6cd6
Mario Gilezan <mariogilezan10@gmail.com>
2022-12-13 15:04:00 +0100

    display flex footer content

--- components/footer.js
+++ components/footer.js
@@ -3,12 +3,12 @@ import ThemeSwitch from './themeSwitch';
 
 export default function Footer(props) {
   return (
-    <Container className='mt-10 border-t border-gray-100 dark:border-gray-800'>
-      <div className='text-sm text-center'>
+    <Container className='mt-10 border-t border-gray-100 dark:border-gray-800 flex flex-col gap-6 items-center'>
+      <ThemeSwitch />
+      <div className='text-sm text-center pb-2'>
         &copy; {new Date().getFullYear()} {props?.copyright}. All rights
         reserved.
       </div>
-      <ThemeSwitch />
     </Container>
   );
 }
