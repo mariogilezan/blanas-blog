f49b0bd257a4ec5ccab2ed05c30718f6e947d5e7
Mario Gilezan <mariogilezan10@gmail.com>
2023-01-03 17:35:52 +0100

    add pagination to all posts

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,41 @@
+import { useState } from 'react';
 import { NextSeo } from 'next-seo';
-import { useRouter } from 'next/router';
 import Container from '../components/container';
 import Layout from '../components/layout';
 import PostList from '../components/postList';
-import { configQuery, postQuery } from '../lib/groq';
+import { configQuery, postsQuery } from '../lib/groq';
 import { getClient } from '../lib/sanity';
 import defaultOG from '../public/img/opengraph.jpg';
 import GetImage from '../utils/getImage';
+import { loadData } from './api/posts';
 
-export default function Home(props) {
-  const { postData: posts, siteConfig, preview } = props;
+const LOAD_MORE_STEP = 8;
 
-  const router = useRouter();
-
+export default function Home({ siteConfig, initialPosts, total }) {
   const ogImage = siteConfig?.openGraphImage
     ? GetImage(siteConfig?.openGraphImage).src
     : defaultOG.src;
+
+  const [posts, setPosts] = useState(initialPosts);
+  const [loadedAmount, setLoadedAmount] = useState(LOAD_MORE_STEP);
+  const [loading, setLoading] = useState(false);
+
+  const showLoadBtn = total > loadedAmount;
+  const getMorePosts = async () => {
+    setLoading(true);
+
+    try {
+      const data = await fetch(
+        `/api/posts?start=${loadedAmount}&end=${loadedAmount + LOAD_MORE_STEP}`
+      ).then((response) => response.json());
+      setLoadedAmount(loadedAmount + LOAD_MORE_STEP);
+      setPosts([...posts, ...data.posts]);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
 
   return (
     <>
@@ -59,6 +79,18 @@ export default function Home(props) {
                 <PostList key={post._id} post={post} aspect='square' />
               ))}
             </div>
+            {showLoadBtn && (
+              <div className='mt-10 flex justify-center'>
+                <button
+                  className='py-2 px-4 bg-green-700 dark:bg-purple-700 text-white rounded-sm hover:bg-green-800 dark:hover:bg-purple-800 transition-color duration-300 ease-in-out active:bg-green-800 dark:active:bg-purple-800  shadow-sm  shadow-green-700/50 
+                  dark:shadow-purple-700/50'
+                  onClick={getMorePosts}
+                  disabled={loading}
+                >
+                  {loading ? 'Încărcare...' : 'Încarcă'}
+                </button>
+              </div>
+            )}
           </Container>
         </Layout>
       )}
@@ -66,16 +98,15 @@ export default function Home(props) {
   );
 }
 
-export async function getStaticProps({ params, preview = false }) {
-  const post = await getClient(preview).fetch(postQuery);
-  const config = await getClient(preview).fetch(configQuery);
+export async function getServerSideProps() {
+  const config = await getClient(false).fetch(configQuery);
+  const { posts: initialPosts, total } = await loadData(0, LOAD_MORE_STEP);
 
   return {
     props: {
-      postData: post,
       siteConfig: { ...config },
-      preview,
+      initialPosts,
+      total,
     },
-    revalidate: 10,
   };
 }
